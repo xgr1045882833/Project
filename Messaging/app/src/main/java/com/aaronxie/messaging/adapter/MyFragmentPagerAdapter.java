fb79a184fce86f35ff471140c70c71efc95d78fd
xgr1045882833 <1045882833@qq.com>
2020-07-16 08:56:27 +0800

    实现首页布局

---
+++ Messaging/app/src/main/java/com/aaronxie/messaging/adapter/MyFragmentPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.aaronxie.messaging.adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * create by Aaron Xie
+ * on 2020/7/15
+ * description:
+ */
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+    private List<Fragment> mFragments;
+    private List<String> mTitles;
+
+    public MyFragmentPagerAdapter(FragmentManager fm, List<Fragment> fragments, List<String> titles) {
+        super(fm);
+        this.mFragments = fragments;
+        this.mTitles = titles;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles.get(position);
+    }
+}
---
+++ Messaging/app/src/main/java/com/aaronxie/messaging/viewmodel/NotificationViewModel.java
@@ -0,0 +1,7 @@
+package com.aaronxie.messaging.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+public class NotificationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
